7e1a7c8d6878c2f2c13ccc3fec21eef92af9d7cb
falewilli <alejandro.willi@gmail.com>
2022-01-27 14:57:16 -0300

    Deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 import logopokemon from "../assets/logoPokemon.png";
 
 export default function Header(){
     return(
         <div className={styles.header}>
-            <img src={logopokemon} alt="Pokemon" width="200px" />
+            <Link to={"/"}>
+                <img src={logopokemon} alt="Pokemon" width="200px" />
+            </Link>
         </div>
     )
 }
